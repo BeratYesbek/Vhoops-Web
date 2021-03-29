07f3d067ab428b6f03fe0a69eb36c7141508aa24
Kerem Sarı <77582858+krmsari@users.noreply.github.com>
2021-03-29 14:05:02 +0300

    i did voyage between page 29.03

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -12,11 +12,6 @@ namespace WebUI.Controllers
         {
             return View();
         }
-
-        public IActionResult About()
-        {
-            return View();
-        }
         public IActionResult Register()
         {
             return View();
