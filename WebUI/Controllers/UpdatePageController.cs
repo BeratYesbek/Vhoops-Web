3fad3ebba519ea53bb957764c5c8c64bd8492842
kerem sarı <kmsriv.185@gmail.com>
2021-04-02 12:36:30 +0300

    controller test

--- WebUI/Controllers/UpdatePageController.cs
+++ WebUI/Controllers/UpdatePageController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebUI.Controllers
 {
-    public class HomeController : Controller
+    public class UpdatePageController : Controller
     {
         public IActionResult Index()
         {
